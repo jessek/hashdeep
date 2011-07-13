a651b2be6323e957c77f05945c304559f77f7087
xchatty <xchatty@6cb38f49-b1f8-427a-8445-eda305bfee05>
2011-07-13 00:35:18 +0000

    resolved

--- win.sh
+++ win.sh
@@ -14,20 +14,19 @@
 # apt-get install libmpfr1ldbl
 # wget http://ppa.launchpad.net/mingw-packages/ppa/ubuntu/pool/main/w/w64-toolchain/x86-64-w64-mingw32-toolchain_1.0b+201011211643-0w2273g93970b22426p16~karmic1_amd64.deb
 # dpkg -i x86-64-w64-mingw32-toolchain_1.0b+201011211643-0w2273g93970b22426p16~karmic1_amd64.deb
-
+#
 # For 32-bit build systems:
 # wget http://ppa.launchpad.net/mingw-packages/ppa/ubuntu/pool/main/w/w64-toolchain/i686-w64-mingw32-toolchain_1.0b+201011211643-0w2273g93970b22426p16~karmic1_amd64.deb
 # dpkg -i i686-w64-mingw32-toolchain_1.0b+201011211643-0w2273g93970b22426p16~karmic1_amd64.deb
 #
 # Source for this tip: http://ubuntuforums.org/showthread.php?t=1705566
 
-# I haven't been able to find a 64-bit cross-compiler for Mac.
-
-
-
 # On Fedora, you can install mingw32 using standard yum.
 # To install mingw64, follow the instructions at:
 # http://fedoraproject.org/wiki/MinGW/CrossCompilerFramework
+# http://fedoraproject.org/wiki/Features/Mingw-w64_cross_compiler
+# http://www.advancedhpc.com/tower_servers/tower_server_products.html
+#
 # $ cd /etc/yum.repos.d
 # $  sudo wget http://fedoraproject.org/wiki/MinGW/CrossCompilerFramework#Development_and_testing_repository
 # $ yum update
@@ -38,6 +37,10 @@
 # http://fedoraproject.org/wiki/MinGW/CrossCompilerFramework
 #
 # sudo yum install mingw32-gcc-c++
+
+# I haven't been able to find a 64-bit cross-compiler for Mac.
+
+
 
 if [ ! -r configure ];
 then
@@ -51,18 +54,32 @@ then
   make distclean
 fi
 
-if i586-mingw32msvc-g++ -v ;
-then 
-  echo I can run the i586-mingw32msvc-g++ compiler
-  ./configure --host=i586-mingw32msvc
-  make
-fi
+echo Trying to make the 32-bit windows
+have32='no'
+for cc in i586-mingw32msvc  i386-mingw32
+do
+  if [ $have32 = 'no' ] && ${cc}-g++ -v ;
+  then 
+    echo I can run the ${cc}-g++ compiler
+    ./configure --host=$cc
+    make
+    ${cc}-strip */*.exe
+    have32='yes'
+  fi
+done
 
-if i386-mingw32-g++ -v ;
-then 
-  echo I can can run i386-mingw32-g++ comipler
-  ./configure --host=i386-mingw32
-  make
-  i386-mingw32-strip src/*.exe
-fi
+echo Trying to make the 64-bit windows
+have64='no'
+for cc in x86_64-w64-mingw32
+do
+  if [ $have64 = 'no' ] && ${cc}-g++ -v ;
+  then 
+    echo I can run the ${cc}-g++ compiler
+    ./configure --host=$cc
+    make
+    ${cc}-strip */*.exe
+    have64='yes'
+done
+echo Successfully compiled 32-bit windows code: $have32
+echo Successfully compiled 64-bit windows code: $have64
 
