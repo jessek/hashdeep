6ddb4d85f3aa26dab681c8fe2a7eff110a8e7905
xchatty <xchatty@6cb38f49-b1f8-427a-8445-eda305bfee05>
2011-09-18 03:09:12 +0000

    builds udner FC and compiles under FC

--- CONFIGURE_FC.sh
+++ CONFIGURE_FC.sh
@@ -22,6 +22,9 @@ if [ $USER != "root" ]; then
   exit 1
 fi
 
+echo wget is required for updating fedora
+yum -y install wget
+
 echo Adding the Fedora Win32 and Win64 packages
 echo For information, please see:
 echo http://fedoraproject.org/wiki/MinGW/CrossCompilerFramework
@@ -29,20 +32,29 @@ if [ ! -d /etc/yum.repos.d ]; then
   echo /etc/yum.repos.d does not exist. This is very bad.
   exit 1
 fi
-cd /etc/yum.repos.d
-if wget http://build1.openftd.org/fedora-cross/fedora-cross.repo ; then
-  echo Cannot download fedora-cross.repo
-  exit 1
+if [ ! -r /etc/yum.repos.d/fedora-cross.repo ] ; then
+  cd /etc/yum.repos.d
+  if wget http://build1.openftd.org/fedora-cross/fedora-cross.repo ; then
+    echo Successfully downloaded
+  else
+    echo Cannot download the file.
+    exit 1
+  fi
 fi
 
 echo Now adding all of the packages that we will need
-yum -y install autoconf automake
+if yum -y install autoconf automake gcc strings mingw32-gcc mingw32-gcc-c++ mingw64-gcc mingw64-gcc-c++ ; then
+  echo All yummy things installed
+else
+  could not install all yumming things.
+  exit 1
+fi
 
 echo 
 echo Now updating yum packages.
 yum -y update
 
-
+echo Getting pthreads
 
 if [ ! -r pthreads-w32-2-8-0-release.tar.gz ]; then
   wget ftp://sourceware.org/pub/pthreads-win32/pthreads-w32-2-8-0-release.tar.gz
@@ -54,7 +66,7 @@ cd pthreads-w32-2-8-0-release
 for CROSS in i686-w64-mingw32 x86_64-w64-mingw32 
 do
   make CROSS=$CROSS- CFLAGS="-DHAVE_STRUCT_TIMESPEC -I." clean GC-static
-  install *.h /usr/$CROSS/sys-root/mingw/include/
+  install implement.h need_errno.h pthread.h sched.h semaphore.h /usr/$CROSS/sys-root/mingw/include/
   if [ $? != 0 ]; then
     echo "Unable to install include files for $CROSS"
     exit 1
