774f41a88ff6123e73582c0b9a5775dffa504c99
jessekornblum <jessekornblum@6cb38f49-b1f8-427a-8445-eda305bfee05>
2012-01-17 03:15:58 +0000

    Updates for Fedora Core 16

--- CONFIGURE_FC.sh
+++ CONFIGURE_FC.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 cat <<EOF
 ****************************************************************
-Configuring FC15 for cross-compiling multi-threaded 32-bit and
+Configuring FC15/16 for cross-compiling multi-threaded 32-bit and
 		 64-bit Windows programs with mingw.
 ****************************************************************
 
-This script will configure a fresh Fedora 15 system to compile with
+This script will configure a fresh Fedora 15 or 16 system to compile with
 mingw32 and 64.  It requires:
 
-1. FC15 installed and running. Typically you will do this by:
+1. Fedora Core installed and running. Typically you will do this by:
 
    1a - download the ISO for the 64-bit DVD (not the live media) from:
         http://fedoraproject.org/en/get-fedora-options#formats
@@ -40,13 +40,13 @@ EOF
 read
 
 if [ $USER != "root" ]; then
-  echo This script must be run as root
+  echo ERROR: You must run this script as root.
   exit 1
 fi
 
 if [ ! -r /etc/redhat-release ]; then
-  echo This requires Fedora Linux
-  echo Please download Fedora-15-x86_64-Live-Desktop.iso.
+  echo ERROR: This script requires Fedora Linux.
+  echo Please download Fedora-16-x86_64-Live-Desktop.iso.
   echo Boot the ISO and chose System Tools / Install to Hard Drive.
   echo You will then need to:
   echo      sudo yum -y install subversion
@@ -56,57 +56,60 @@ if [ ! -r /etc/redhat-release ]; then
 fi
 
 if grep 'Fedora.release.15' /etc/redhat-release ; then
-  echo Fedora Release 15 detected
+  echo Detected Fedora Core Release 15, ok.
+elif grep 'Fedora.release.16' /etc/redhat-release ; then
+  echo Detected Fedora Core Release 16, good.
 else
-  echo This script is only tested for Fedora Release 15
+  echo
+  echo ERROR: Unsupported operating system. Try using Fedora Core 16.
   exit 1
 fi
 
-if [ $USER != "root" ]; then
-  echo This script must be run as root
-  exit 1
-fi
-
-echo Will now try to install 
-
-echo wget is required for updating fedora
+echo Attempting to install tools necessary to build md5deep...
+echo Installing wget...
 yum -y install wget
+echo Successfully installed wget.
 
-echo Adding the Fedora Win32 and Win64 packages
+echo Installing Fedora Win32 and Win64 cross compiler packages...
 echo For information, please see:
 echo http://fedoraproject.org/wiki/MinGW/CrossCompilerFramework
 if [ ! -d /etc/yum.repos.d ]; then
-  echo /etc/yum.repos.d does not exist. This is very bad.
+  echo ERROR: /etc/yum.repos.d does not exist. This is very bad. I quit.
   exit 1
 fi
 if [ ! -r /etc/yum.repos.d/fedora-cross.repo ] ; then
   if wget --directory-prefix=/etc/yum.repos.d  http://build1.openftd.org/fedora-cross/fedora-cross.repo ; then
-    echo Successfully downloaded
+    echo Successfully downloaded repository list for cross compilers.
   else
-    echo Cannot download the file.
+    echo ERROR: Could not download the repository list cross compilers.
     exit 1
   fi
 fi
 
-echo Now adding all of the packages that we will need
+echo Now adding all of the packages that we will need...
 if yum -y install autoconf automake gcc strings mingw32-gcc mingw32-gcc-c++ mingw64-gcc mingw64-gcc-c++ ; then
-  echo All yummy things installed
+  echo Successfully installed all required dependencies.
 else
-  could not install all yumming things.
+  echo ERROR: Could not install required dependencies.
   exit 1
 fi
 
 echo 
-echo Now updating yum packages.
+echo Updating all yum packages. This may take a little while...
 yum -y update
 
-echo Getting pthreads
+echo
+echo Getting pthreads...
+echo
 if [ ! -r pthreads-w32-2-8-0-release.tar.gz ]; then
   wget ftp://sourceware.org/pub/pthreads-win32/pthreads-w32-2-8-0-release.tar.gz
 fi
 /bin/rm -rf pthreads-w32-2-8-0-release 
 tar xfvz pthreads-w32-2-8-0-release.tar.gz
 
+echo
+echo Compiling pthreads...
+echo
 pushd pthreads-w32-2-8-0-release
   for CROSS in i686-w64-mingw32 x86_64-w64-mingw32 
   do
@@ -124,19 +127,27 @@ pushd pthreads-w32-2-8-0-release
     make clean
   done
 popd
+echo
+echo Successfully installed pthreads.
 
 echo ================================================================
 echo ================================================================
 echo
-echo Now downloading md5deep SVN repository
-svn co https://xchatty@md5deep.svn.sourceforge.net/svnroot/md5deep
-if [ ! -d md5deep/branches/version4 ] ; then
-  echo md5deep subversion repository was not downloaded.
-  exit 1
-fi
-here=`pwd`
-cd md5deep/branches/version4
-sh bootstrap.sh
-./configure
-make windist
-mv *.zip "$here"
+echo Installation complete!
+echo You should now be able to cross compile the programs.
+echo $ sh bootstrap.sh
+echo $ ./configure
+echo $ make world
+
+#echo Now downloading md5deep SVN repository
+#svn co https://xchatty@md5deep.svn.sourceforge.net/svnroot/md5deep
+#if [ ! -d md5deep/branches/version4 ] ; then
+#  echo md5deep subversion repository was not downloaded.
+#  exit 1
+#fi
+#here=`pwd`
+#cd md5deep/branches/version4
+#sh bootstrap.sh
+#./configure
+#make windist
+#mv *.zip "$here"
